f6b1191b54c342fb9e13b554ff550db8ac051eb7
Stefan Kupstaitis-Dunkler <stefan.dun@gmail.com>
2017-08-02 23:57:19 +0200

    fix wrong nifipy module name

--- restart_controller.py
+++ restart_controller.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     from nifipy import ControllerService
     import logging
     logging.basicConfig(level=logging.ERROR)
-    logging.getLogger(nifiapi.__name__).setLevel(logging.INFO)
+    logging.getLogger(nifipy.__name__).setLevel(logging.INFO)
 
     url_base = os.environ["URL_BASE"] 
     controller_id = os.environ["CONTROLLER_ID"]
